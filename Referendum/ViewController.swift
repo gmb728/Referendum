22e8b2cc60f80a1285601639c55a648daba11b85
Chang Sophia <you@example.com>
2018-11-29 14:18:58 +0800

    Initial Commit

---
+++ Referendum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Referendum
+//
+//  Created by Chang Sophia on 11/29/18.
+//  Copyright © 2018 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
